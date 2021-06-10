873b0a09ee117a4b30e48ef0783dd4f2e45fd774
ndgigliotti <ndgigliotti@gmail.com>
2021-06-10 16:35:49 -0400

    add SCS wrapper

--- tools/modeling/selection.py
+++ tools/modeling/selection.py
@@ -1,9 +1,20 @@
+import textwrap
+from typing import List, Union
+
+import numpy as np
 import pandas as pd
+from feature_engine.selection import \
+    SmartCorrelatedSelection as SmartCorrelatedSelectionFE
+from IPython.core.display import HTML
+from IPython.display import display
 from sklearn.base import clone
 from sklearn.model_selection import GridSearchCV, RandomizedSearchCV
 from sklearn.pipeline import Pipeline
+from sklearn.utils.validation import check_is_fitted
 
 from .. import utils
+
+Variables = Union[None, int, str, List[Union[str, int]]]
 
 
 def tidy_results(cv_results: dict) -> pd.DataFrame:
@@ -82,3 +93,58 @@ def make_search_pipe(
     search_pipe = list(clone(estimator_pipe[:-1]).named_steps.items())
     search_pipe.append((step_name, search))
     return Pipeline(search_pipe)
+
+
+class SmartCorrelatedSelection(SmartCorrelatedSelectionFE):
+    """Wrapper for feature_engine.selection.SmartCorrelatedSelection."""
+
+    def __init__(
+        self,
+        variables: Variables = None,
+        method: str = "pearson",
+        threshold: float = 0.8,
+        missing_values: str = "ignore",
+        selection_method: str = "missing_values",
+        estimator=None,
+        scoring: str = "roc_auc",
+        cv: int = 3,
+        verbose: bool = False,
+    ):
+        super().__init__(
+            variables=variables,
+            method=method,
+            threshold=threshold,
+            missing_values=missing_values,
+            selection_method=selection_method,
+            estimator=estimator,
+            scoring=scoring,
+            cv=cv,
+        )
+        self.verbose = verbose
+
+    @property
+    def selected_features_(self):
+        check_is_fitted(self)
+        corr_superset = set().union(*self.correlated_feature_sets_)
+        return list(corr_superset.difference(self.features_to_drop_))
+
+    def show_report(self):
+        check_is_fitted(self)
+        name = self.__class__.__name__
+        info = [
+            pd.Series(self.selected_features_, name="Selected"),
+            pd.Series(self.features_to_drop_, name="Rejected"),
+        ]
+        info = pd.concat(info, axis=1)
+        # info = info.applymap(lambda x: f"'{x}'", na_action="ignore")
+        name = self.__class__.__name__
+        # info = info.T.to_string(na_rep="", header=False, justify="left", max_cols=6)
+        info = info.T.to_html(na_rep="", header=False, max_cols=6, notebook=True)
+        info = f"<h4>{name}</h4>{info}"
+        display(HTML(info))
+
+    def fit(self, X: pd.DataFrame, y: pd.Series = None):
+        super().fit(X, y=y)
+        if self.verbose:
+            self.show_report()
+        return self
